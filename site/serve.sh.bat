b8212fc61b37997a13b442a2dda6b03cc8bde930
Alonso Rodriguez <alonso.rodriguez@udc.es>
2024-02-18 09:39:14 +0100

    Update serve.sh.bat

--- site/serve.sh.bat
+++ site/serve.sh.bat
@@ -3,10 +3,10 @@
 GOTO :CMDSCRIPT
 ::CMDLITERAL
 
-echo "Executing a Python http server in https://localhost:8080/site.html"
+echo "Executing a Python http server in http://localhost:8080/site.html"
 python -m http.server 8080
 exit $?
 
 :CMDSCRIPT
-echo Executing a Python http server in https://localhost:8080/site.html
+echo Executing a Python http server in http://localhost:8080/site.html
 python.exe -m http.server 8080
